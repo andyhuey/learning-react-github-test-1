83dbdf1d01378edcf3e1c02d32be4bdbec6ad565
Andrew Huey <andyhuey@users.noreply.github.com>
2020-06-26 16:15:47 -0400

    Handling Promise States

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+// const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
+// const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
 function GitHubUser({ login }) {
-  const [data, setData] = useState(loadJSON(`user:${login}`));
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (!data) return;
-    console.log(`login=${login}`);
-    console.log(`data.login=${data.login}`);
-    if (data.login !== login) return;
-    const { name, avatar_url, location } = data;
-    console.log(`Saving data for ${login}, ${name}.`);
-    saveJSON(`user:${login}`, {
-      name,
-      login,
-      avatar_url,
-      location
-    });
-  }, [data, login]);
-
-  useEffect(() => {
+useEffect(() => {
     if (!login) return;
-    if (data && data.login === login) return;
-    console.log(`Fetching data for ${login}.`);
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
+      .then(data => data.json())
       .then(setData)
-      .catch(console.error);
-  }, [login, data]);
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
 
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  if (loading) return <h1>loading...</h1>;
+  if (error)
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
-  return null;
+  return (
+    <div className="githubUser">
+      <img
+        src={data.avatar_url}
+        alt={data.login}
+        style={{ width: 200 }}
+      />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
 }
 
 export default function App() {
